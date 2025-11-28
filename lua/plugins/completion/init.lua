b25ced7697dd59771676dbe039ed645b2541c6ed
altair <luca.barra@edu.unito.it>
2025-11-28 22:18:12 +0100

    fix icons error

--- lua/plugins/completion/init.lua
+++ lua/plugins/completion/init.lua
@@ -30,32 +30,6 @@ return {
 			completion = {
 				menu = {
 					border = "double",
-					draw = {
-						components = {
-							kind_icon = {
-								text = function(ctx)
-									local icon = ctx.kind_icon
-									if vim.tbl_contains({ "Path" }, ctx.source_name) then
-										local dev_icon, _ = require("mini.icons").get_icon(ctx.label)
-										if dev_icon then
-											icon = dev_icon
-										end
-									end
-									return icon .. ctx.icon_gap
-								end,
-								highlight = function(ctx)
-									local hl = ctx.kind_hl
-									if vim.tbl_contains({ "Path" }, ctx.source_name) then
-										local dev_icon, dev_hl = require("mini.icons").get_icon(ctx.label)
-										if dev_icon then
-											hl = dev_hl
-										end
-									end
-									return hl
-								end,
-							},
-						},
-					},
 				},
 				documentation = { auto_show = true, window = { border = "double" } },
 			},
